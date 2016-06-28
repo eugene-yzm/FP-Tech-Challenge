8fccc10e91b52e54e045d60d96d1a89e8840485e
Eugene M <symbato71@gmail.com>
2016-06-28 02:27:58 +0100

    Using get api to populate list on webpage

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,21 +2,37 @@
 
 /* Controller */
 angular.module('myApp.controllers', []).
-	controller('AppCtrl', function ($scope, $http) {
+	controller('AppCtrl', function ($scope, $http, $window) {
 		// Insert controller code here
-	$scope.formInfo = {};	
+		$scope.formInfo = {};	
 
-	console.log('asdf');
-	$scope.submitForm = function() {
-        console.log($scope.formInfo);
-	$http.post('/api/quote', $scope.formInfo)
-		.success(function(response){
-			 //your code in case the post succeeds
-			 console.log('Success');
-			 console.log(response);
-		 })
-		.error(function(err){
-			 //your code in case your post fails
-			 console.log(err);
-		 });
-	}});
+		$window.onload = function() {
+			console.log('Initializing Webpage');
+			$http.get('/api/apparel?all')
+			.success(function(response){
+				 //your code in case the post succeeds
+				 console.log('Get all success');
+				 var resp = response;
+				 $scope.shirts = resp;
+			 })			
+			.error(function(err){
+				 //your code in case your post fails
+				 console.log(err);
+			 });
+		};
+
+		$scope.submitForm = function() {
+			console.log($scope.formInfo);
+			$http.post('/api/quote', $scope.formInfo)
+			.success(function(response){
+				 //your code in case the post succeeds
+				 console.log('Success');
+				 console.log(response);
+			 })
+			.error(function(err){
+				 //your code in case your post fails
+				 console.log(err);
+			 });
+		}
+	
+	});
